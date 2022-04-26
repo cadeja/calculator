160c25ef6f4f68888bd44069d37ce241ced45062
Cade <cjayenn@gmail.com>
2022-04-26 12:27:06 -0400

    added 'add' event listener

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 
 const numpadButtons = document.querySelectorAll(".numpad-button");
+const add = document.querySelector("#add");
+const subtract = document.querySelector("#subtract");
+const multiply = document.querySelector("#multiply");
+const divide = document.querySelector("#divide");
+
 const input = document.querySelector("#input-box");
 
+// numpad buttons event listener
+// pushes text content of buttons to the input field
 for (let i = 0; i < numpadButtons.length; i++){
     numpadButtons[i].addEventListener('click', () => {
         input.textContent += (numpadButtons[i].textContent);
     });
 }
 
+add.addEventListener("click", () =>{
+    input.textContent += " + ";
+});
